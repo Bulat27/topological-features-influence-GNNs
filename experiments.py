8dfa62db3a8a74d0447443dabf82f6dd5a359ba8
Nikola Bulat <nbulat99@gmail.com>
2023-12-25 18:25:28 +0100

    Add training runtime measurement to run_experiments

--- experiments.py
+++ experiments.py
@@ -1,22 +1,28 @@
 import torch
+import time
 
 def run_experiments(model, data, n_runs, n_epochs, optimizer, criterion, device):
   data = data.to(device)
   model = model.to(device)
-  test_accs = []
+  test_accs, run_times = [], []
 
   for i in range(1 , n_runs + 1):
     print(f"\n RUN: {i}\n")
 
     reset_weights(model)
+
+    start_time = time.time()
     train_losses, train_accs, val_losses, val_accs, best_epoch = model_training(n_epochs, model, data, optimizer, criterion)
+    end_time = time.time()
+
+    run_times.append(end_time - start_time)
 
     model.load_state_dict(torch.load('best_model.pth'))
     test_acc, _  = eval(model, data, data.test_mask, criterion)
     test_accs.append(test_acc)
     avg_acc = sum(test_accs) / len(test_accs)
 
-  return avg_acc, test_accs, train_losses, train_accs, val_losses, val_accs, best_epoch
+  return avg_acc, test_accs, train_losses, train_accs, val_losses, val_accs, run_times, best_epoch
 
 def reset_weights(model):
     for layer in model.children():
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 # z-score normalization of a tensor
 def NormalizeTensor(data):
@@ -22,3 +23,19 @@ def ZScoreNormalization(x):
   for key in x.keys():
     x[key] = float((x[key]-x_mean)/x_std)
   return x
+
+def plot_results(n_epochs, train_losses, train_accs, val_losses, val_accs):
+  N_EPOCHS = n_epochs
+  # Plot results
+  plt.figure(figsize=(20, 6))
+  _ = plt.subplot(1,2,1)
+  plt.plot(np.arange(N_EPOCHS)+1, train_losses, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS)+1, val_losses, linewidth=3)
+  _ = plt.legend(['Train', 'Validation'])
+  plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Loss')
+
+  _ = plt.subplot(1,2,2)
+  plt.plot(np.arange(N_EPOCHS)+1, train_accs, linewidth=3)
+  plt.plot(np.arange(N_EPOCHS)+1, val_accs, linewidth=3)
+  _ = plt.legend(['Train', 'Validation'])
+  plt.grid('on'), plt.xlabel('Epoch'), plt.ylabel('Accuracy')
