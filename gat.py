2f347275362912259d2063e55ef25a16396ee931
Nikola Bulat <nbulat99@gmail.com>
2023-12-27 14:16:40 +0100

    Add GATPre and GCNPre (models with MLP preprocessing layer)

--- gat.py
+++ gat.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # The number of hidden_channels is multiplied by the number of heads.
-class GAT(torch.nn.Module):
+class GATBase(torch.nn.Module):
     def __init__(self, data, hidden_channels, heads):
         super().__init__()
         # torch.manual_seed(42)
@@ -19,3 +19,28 @@ class GAT(torch.nn.Module):
         x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv2(x, edge_index)
         return x
+    
+class GATPre(torch.nn.Module):
+    def __init__(self, data, hidden_channels, heads, mlp_hidden_channels):
+        super().__init__()
+
+        # Single-layer MLP preprocessing
+        self.mlp_linear = nn.Linear(data.num_features, mlp_hidden_channels)
+
+        # GAT layers
+        self.conv1 = GATConv(in_channels=mlp_hidden_channels, out_channels=hidden_channels, heads=heads, dropout=0.6)
+        self.conv2 = GATConv(in_channels=hidden_channels*heads, out_channels=data.num_classes, heads=1, dropout=0.6)
+
+    def forward(self, x, edge_index):
+        # MLP preprocessing layer
+        x = self.mlp_linear(x)
+        x = F.relu(x)  
+
+        # GAT layers
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return x    
--- gcn.py
+++ gcn.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+import torch.nn as nn
 
-class GCN(torch.nn.Module):
+class GCNBase(torch.nn.Module):
     def __init__(self, data, hidden_channels):
         super().__init__()
         self.conv1 = GCNConv(data.num_features, hidden_channels)
@@ -14,3 +15,25 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
         return x
+
+class GCNPre(torch.nn.Module):
+    def __init__(self, data, hidden_channels, mlp_hidden_channels):
+        super().__init__()
+
+        # Single-layer MLP preprocessing
+        self.mlp_linear = nn.Linear(data.num_features, mlp_hidden_channels)
+
+        self.conv1 = GCNConv(mlp_hidden_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, data.num_classes)
+
+    def forward(self, x, edge_index):
+        # MLP preprocessing layer
+
+        x = self.mlp_linear(x)
+        x = F.relu(x)  
+
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x        
