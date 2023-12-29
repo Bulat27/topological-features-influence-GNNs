11a581089a0a9f95322bb5028d176dbb27c4a094
Alessandro Resta <77611890+Alessandrus00@users.noreply.github.com>
2023-12-29 21:52:18 +0100

    add features combinations

--- experiments.py
+++ experiments.py
@@ -34,17 +34,37 @@ def reset_weights(model):
             raise AttributeError(f'The layer {layer.__class__.__name__} does not have a reset_parameters method.')
         
 def run_feature_combinations(original_features, structural_features, positional_features, file_name, normalization=lambda x: x):
-    features_combinations = [original_features, structural_features, positional_features] # TODO: Add the concatenation of features
-    file_names = ['original', 'structural', 'positional', 'original-structural', 'original-positional', 'structural-positional', 'original-structural-positional']
-    basic_models = dict()
+    features_combinations = [
+      original_features, 
+      structural_features, 
+      positional_features, 
+      concatenate(original_features,structural_features), 
+      concatenate(original_features,positional_features),
+      concatenate(structural_features,positional_features),
+      concatenate(original_features,structural_features,positional_features)]
 
+    file_names = [
+      'original', 
+      'structural', 
+      'positional', 
+      'original-structural', 
+      'original-positional', 
+      'structural-positional', 
+      'original-structural-positional']
+
+    basic_models = dict()
     for curr_features, curr_file_name in zip(features_combinations, file_names):
         data.x = curr_features
         data.x = normalization(data.x)
         results = dict()
-        results['avg_acc'], results['test_accs'], results['train_losses'], results['train_accs'], results['val_losses'], results['val_accs'], results['run_times'],
+        results['avg_acc'], 
+        results['test_accs'], 
+        results['train_losses'], 
+        results['train_accs'], 
+        results['val_losses'], 
+        results['val_accs'], 
+        results['run_times'],
         results['best_epoch'] = experiments.run_experiments(model, data, n_runs, n_epochs, optimizer, criterion, device) # These should be "global variables"
-
         results['model'] = model
      
         basic_models[curr_file_name] = results
